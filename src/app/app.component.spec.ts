a161dad99006b01c8ac2fcdffacf78aca85dadc4
Boris Yakubchik <yboris@yahoo.com>
2023-12-22 12:12:24 -0500

    modernize test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -7,44 +6,18 @@ describe('AppComponent', () => {
   beforeEach(() =>{
     component = new AppComponent();
   });
-=======
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
->>>>>>> master
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have as title 'an-qrcode-demo'`, () => {
     expect(component.title).toEqual('an-qrcode-demo');
-=======
-  it(`should have as title 'NQRCode'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('NQRCode');
->>>>>>> master
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-<<<<<<< HEAD
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('qrcode')).toBeTruthy();
-=======
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('NQRCode app is running!');
->>>>>>> master
   });
 });
